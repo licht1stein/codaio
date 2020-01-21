28f91a34ea86c173fb70ff466750ca46bc6151ca
Cedric Hofstetter <cedric.hofstetter@mac.com>
2020-01-21 21:58:25 +0200

    tests: add linting to noxfile

--- noxfile.py
+++ noxfile.py
@@ -6,3 +6,9 @@ def tests(session):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
+
+
+@nox.session
+def lint(session):
+    session.install("flake8")
+    session.run("flake8", "codaio", "tests")
