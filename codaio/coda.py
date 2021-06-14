d4da68ef6cbd506bfdaaca1de9638b59cd21fe33
Caroline Simpson <bitbucket@hoojiboo.com>
2021-06-14 16:39:40 -0400

    Handle formula attribute on column json (#43)
    * Adding the formula attribute to the Column object
    
    * Add a column with a formula to the tests.

--- codaio/coda.py
+++ codaio/coda.py
@@ -1067,6 +1067,7 @@ class Column(CodaObject):
     table: Table = attr.ib(repr=False)
     display: bool = attr.ib(default=None, repr=False)
     calculated: bool = attr.ib(default=False)
+    formula: str = attr.ib(default=None, repr=False)
 
 
 @attr.s(auto_attribs=True, hash=True)
